04c4586af9a83417b7c51005d30c083a903b0dcf
blue-beaker <yyy5566778899@126.com>
2024-11-28 11:06:47 +0800

    info

--- src/main/java/io/bluebeaker/bettertradingmenu/BetterTradingMenuConfig.java
+++ src/main/java/io/bluebeaker/bettertradingmenu/BetterTradingMenuConfig.java
@@ -5,7 +5,7 @@ import net.minecraftforge.common.config.Config.Comment;
 import net.minecraftforge.common.config.Config.LangKey;
 import net.minecraftforge.common.config.Config.Type;
 
-@Config(modid = BetterTradingMenu.MODID,type = Type.INSTANCE,category = "general")
+@Config(modid = BetterTradingMenuMod.MODID,type = Type.INSTANCE,category = "general")
 public class BetterTradingMenuConfig {
     @Comment({"Move items when clicked a trade in this mod's menu.",
     "when disabled, clicking a trade only selects it in the GUI."})
